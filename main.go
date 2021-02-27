6562efedf918dbb311be6a6b2bdafa77452d0f3a
Clint J Edwards <clint.j.edwards@gmail.com>
2021-02-26 19:39:33 -0500

    This algo is faster

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	term "github.com/nsf/termbox-go"
@@ -16,13 +16,16 @@ import (
 
 // plug is the representation of the keybinding and plug pairing
 type plug struct {
-	IPAddress string
-	KeyMap    string
-	Model     string
-	Name      string
-	On        bool
+	IPAddress  string
+	TriggerKey int
+	Model      string
+	Name       string
+	mtx        *sync.Mutex
+	On         bool
+	lastCmd    time.Time
 }
 
+// all of the structs below are just to conform to the sysinfo json result
 type system struct {
 	command `json:"system"`
 }
@@ -61,31 +64,42 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer term.Close()
 
 	// mapping should be in the form: <ip addr>:<key>,<ip addr>:<key>
 	mapping := os.Args[1]
-	_ = processMapping(mapping)
+	plugs := processMapping(mapping)
+	getSystemInfo(plugs...)
 
 	for {
-		switch ev := term.PollEvent(); ev.Type {
-		case term.EventKey:
-			switch ev.Key {
-			case term.KeyArrowLeft:
-				fmt.Println("You pressed the left key")
-			case term.KeyArrowDown:
-				fmt.Println("You pressed the right key")
-			case term.KeyCtrlC:
-				return
+		fmt.Println("Listening for input")
+		event := term.PollEvent()
+		eventType := event.Type
+
+		if eventType != term.EventKey {
+			continue
+		}
+
+		if event.Key == term.KeyCtrlC {
+			return
+		}
+
+		for _, plug := range plugs {
+			if term.Key(plug.TriggerKey) == event.Key {
+				_ = term.Sync()
+				err := plug.toggle()
+				if err != nil {
+					fmt.Printf("could not toggle switch %s; %v", plug.Name, err)
+					continue
+				}
+
 			}
 		}
 	}
-	//getSystemInfo(plugs...)
 }
 
 // This takes a long time.
 func getSystemInfo(plugs ...*plug) {
-	fmt.Println("Retrieving information for plugs; this might take a while")
-
 	for _, plug := range plugs {
 		info, err := plug.systemInfo()
 		if err != nil {
@@ -104,38 +118,6 @@ func int2bool(r int) bool {
 	return r == 1
 }
 
-func encrypt(plaintext string) []byte {
-	n := len(plaintext)
-	buf := new(bytes.Buffer)
-	_ = binary.Write(buf, binary.BigEndian, uint32(n))
-	ciphertext := []byte(buf.Bytes())
-
-	key := byte(0xAB)
-	payload := make([]byte, n)
-	for i := 0; i < n; i++ {
-		payload[i] = plaintext[i] ^ key
-		key = payload[i]
-	}
-
-	for i := 0; i < len(payload); i++ {
-		ciphertext = append(ciphertext, payload[i])
-	}
-
-	return ciphertext
-}
-
-func decrypt(ciphertext []byte) []byte {
-	n := len(ciphertext)
-	key := byte(0xAB)
-	var nextKey byte
-	for i := 0; i < n; i++ {
-		nextKey = ciphertext[i]
-		ciphertext[i] = ciphertext[i] ^ key
-		key = nextKey
-	}
-	return ciphertext
-}
-
 func processMapping(m string) []*plug {
 	mappingSlice := strings.Split(m, ",")
 
@@ -143,9 +125,14 @@ func processMapping(m string) []*plug {
 
 	for _, mapping := range mappingSlice {
 		IPKeyPair := strings.Split(mapping, ":")
+		triggerKey, err := strconv.Atoi(IPKeyPair[1])
+		if err != nil {
+			panic(err)
+		}
 		plugs = append(plugs, &plug{
-			IPAddress: IPKeyPair[0],
-			KeyMap:    IPKeyPair[1],
+			IPAddress:  IPKeyPair[0],
+			TriggerKey: triggerKey,
+			mtx:        &sync.Mutex{},
 		})
 	}
 
@@ -154,14 +141,12 @@ func processMapping(m string) []*plug {
 
 func (p *plug) systemInfo() (system, error) {
 	payload := `{"system":{"get_sysinfo":{}}}`
-	data := encrypt(payload)
-	reading, err := send(p.IPAddress, data)
+	results, err := p.sendCmd(payload)
 	if err != nil {
 		return system{}, err
 	}
 
 	var info system
-	results := decrypt(reading[4:])
 	err = json.Unmarshal(results, &info)
 	if err != nil {
 		return system{}, err
@@ -170,48 +155,96 @@ func (p *plug) systemInfo() (system, error) {
 	return info, nil
 }
 
-func (p *plug) toggle() (err error) {
-	if p.On {
-		err = p.turnOff()
-		return
-	}
-
-	err = p.turnOn()
-	return
-}
-
 func (p *plug) turnOn() (err error) {
-	json := `{"system":{"set_relay_state":{"state":1}}}`
-	data := encrypt(json)
-	_, err = send(p.IPAddress, data)
+	payload := `{"system":{"set_relay_state":{"state":1}}}`
+	_, err = p.sendCmd(payload)
 	return
 }
 
 func (p *plug) turnOff() (err error) {
-	json := `{"system":{"set_relay_state":{"state":0}}}`
-	data := encrypt(json)
-	_, err = send(p.IPAddress, data)
+	payload := `{"system":{"set_relay_state":{"state":0}}}`
+	_, err = p.sendCmd(payload)
 	return
 }
 
-func send(ip string, payload []byte) (data []byte, err error) {
-	conn, err := net.DialTimeout("tcp", ip+":9999", time.Duration(10)*time.Second)
-	if err != nil {
-		fmt.Println("Cannot connect to plug:", err)
-		data = nil
+func (p *plug) toggle() (err error) {
+	if p.On {
+		err = p.turnOff()
+		p.On = false
+		fmt.Printf("Toggled: %s %s\n", p.Name, time.Now().Format("01-02 15:04:05"))
 		return
 	}
 
-	_, err = conn.Write(payload)
-	if err != nil {
-		fmt.Println("Cannot connect to plug:", err)
-		data = nil
-		return
-	}
-
-	data, err = ioutil.ReadAll(conn)
-	if err != nil {
-		fmt.Println("Cannot read data from plug:", err)
-	}
+	err = p.turnOn()
+	p.On = true
+	fmt.Printf("Toggled: %s %s\n", p.Name, time.Now().Format("01-02 15:04:05"))
 	return
+}
+
+// sendCmd handles the communication with the plug.
+func (p *plug) sendCmd(data string) ([]byte, error) {
+	// protect against sending too many commands at once
+	p.mtx.Lock()
+	defer func() {
+		p.lastCmd = time.Now()
+		p.mtx.Unlock()
+	}()
+	if time.Since(p.lastCmd) < time.Millisecond*500 {
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	res := make([]byte, 2048)
+
+	// connect to plug
+	conn, err := net.Dial("tcp", p.IPAddress+":9999")
+	if err != nil {
+		return res, fmt.Errorf("connecting to plug: %w", err)
+	}
+	defer conn.Close()
+
+	// set timeout
+	if err := conn.SetDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		return res, fmt.Errorf("setting timeout: %w", err)
+	}
+
+	payload := encrypt([]byte(data))
+
+	if _, err := conn.Write(payload); err != nil {
+		return res, fmt.Errorf("writing payload: %w", err)
+	}
+
+	// receive, decrypt response
+	i, err := conn.Read(res)
+	if err != nil {
+		return res, err
+	}
+	decrypted := decrypt(res[:i]) // only include the bytes that were read
+	return decrypted, nil
+}
+
+// encrypt follows the autokey cipher used by the HS1xx to encrypt commands.
+func encrypt(bx []byte) []byte {
+	key := 171
+	res := make([]byte, 4)
+	binary.BigEndian.PutUint32(res, uint32(len(bx))) // equivalent in python: struct.pack('>I', len(cmd))
+
+	for i := range bx {
+		b := key ^ int(bx[i])
+		key = b
+		res = append(res, byte(b))
+	}
+	return res
+}
+
+// decrypt follows the autokey cipher used by the HS1xx to decrypt commands.
+func decrypt(bx []byte) []byte {
+	key := 171
+	var res []byte
+
+	for i := 4; i < len(bx); i++ { // first 4 bytes are padding
+		b := key ^ int(bx[i])
+		key = int(bx[i])
+		res = append(res, byte(b))
+	}
+	return res
 }
